3c5db182fa0038808feb0af70355b22f723051c4
lessthanpi <69544522+lessthanpi@users.noreply.github.com>
2020-09-16 13:02:59 -0500

    Add files via upload

---
+++ Exploritory Data Analysis - Project 1.R
@@ -0,0 +1,106 @@
+# Coursera Exploratory Data Analysis Proj 1 #
+
+#I try to comment everything!  Sorry if its annoying.
+
+
+
+#####################################################
+# Part I:  Download, unzip, import, and filter data #
+#####################################################
+
+
+##provided link from instructions ##
+
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##grab the file from url ##
+
+download.file(url,destfile="./data2/coursera.zip")
+
+## since it's all zipped, we call this function ##
+
+unzip(zipfile="./data2/coursera.zip",exdir="./data2")
+
+##put it in a table, needs sep, and header ##
+
+df <- read.table("./data2/household_power_consumption.txt"
+                 ,sep=";",header=T)
+
+## have a look  ##
+
+dim(df)   #output:  2075259       9     HUGE!!
+head(df)
+
+##suggested refinement from assignment ##
+
+feb_1_2007 <- df[df$Date == "1/2/2007", ]
+feb_2_2007 <- df[df$Date == "2/2/2007", ]
+table_dates <- rbind(feb_1_2007,feb_2_2007)
+
+dim(table_dates)  #output:   2880    9    Much Better!
+
+#####################################################
+# End of Part I
+#####################################################
+
+
+#####################################################
+# Part II:  Plot 1
+#####################################################
+
+#pull the column we need to put in the histogram
+hist_data <- as.numeric(table_dates$Global_active_power)
+
+#setup the y-axis
+y_ticks <- c(0,1200)
+
+#call the histogram with all the parameters requested
+hist(hist_data,col="red",main="Global Active Power",
+     xlab = "Global Active Power (Kilowatts)",ylim=y_ticks)
+
+#save the picture to disk with requested resolution and file type
+png(file="./data2/plot1.png",
+    width=480, height=480)
+hist(hist_data,col="red",main="Global Active Power",
+     xlab = "Global Active Power (Kilowatts)",ylim=y_ticks)
+dev.off()
+
+#####################################################
+# End of Part II
+#####################################################
+
+
+#####################################################
+# Part III:  Plot 2
+#####################################################
+
+
+#make the time column from feb1 usable numeric
+Thursday <- gsub(":","",feb_1_2007$Time)
+Thursday <- as.numeric(Thursday)
+
+#same for next day
+Friday <- gsub(":","",feb_2_2007$Time)
+Friday <- as.numeric(Friday)
+#Friday needs to start with its "zero" after Thursday
+Friday <- Friday +236000
+
+#glue the days back together for proper list length
+days <- append(Thursday,Friday)
+
+#make the requested line graph with paramaters
+plot(days,hist_data,type="l",xaxt="none",xlab="",
+     ylab="Global Active Power (Kilowatts)")
+axis(1, at=c(1,235900,471900), labels=c("Thu","Fri","Sat"))
+
+#save the picture to disk with requested resolution and file type
+png(file="./data2/plot2.png",
+    width=480, height=480)
+plot(days,hist_data,type="l",xaxt="none",xlab="",
+     ylab="Global Active Power (Kilowatts)")
+axis(1, at=c(1,235900,471900), labels=c("Thu","Fri","Sat"))
+dev.off()
+
+#####################################################
+# End of Part III
+#####################################################
